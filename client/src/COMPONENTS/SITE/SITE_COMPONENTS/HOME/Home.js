337b48a29fc842d710f90bee530055031ba7542b
Chulps <chuckoward@gmail.com>
2020-01-25 11:12:18 -0800

    fixed font in inputs

---
+++ client/src/COMPONENTS/SITE/SITE_COMPONENTS/HOME/Home.js
@@ -0,0 +1,233 @@
+// Home.js
+import React from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "./home.css";
+import Laptop from "../../../../IMG/laptop.png";
+
+// import Me from '../../../../IMG/chuck.png'// just change the name 'chuck' in this import to make it be you instead
+import Wavy from "./HOME_COMPONENTS/Wavy";
+import Clicktool from "../../../../VIDEO/clicktool_dashboard.mp4";
+import ClicktoolLogo from "../../../../IMG/ClicktoolLogoHorizontal";
+import ClicktoolCity from "./HOME_COMPONENTS/ClicktoolCity";
+
+const images = require.context("../../../../IMG/USERS", true);
+const styles = {
+  userInput: {
+    marginBottom: "10px",
+    height: "50px",
+    width: "300px",
+    padding: "10px",
+    fontSize: "24px",
+    textAlign: "center"
+  }
+};
+
+class Home extends React.Component {
+  state = {
+    user: "",
+    name: "",
+    profile: "",
+    userNotFound: false
+  };
+
+  componentDidMount() {
+    axios.get(`getUser/chuck`).then(data => {
+      this.setState(
+        {
+          profile: data.data
+        },
+        () => {
+          if (!this.state.profile) {
+            this.setState({
+              userNotFound: true
+            });
+          }
+        }
+      );
+    });
+  }
+
+  handleUserInput = e => {
+    this.setState({
+      name: e.target.value
+    });
+  };
+
+  submitUserInput = () => {
+    this.setState(
+      {
+        user: this.state.name.toLowerCase()
+      },
+      () => {
+        axios.get(`getUser/${this.state.user}`).then(data => {
+          this.setState(
+            {
+              profile: data.data
+            },
+            () => {
+              if (!this.state.profile) {
+                this.setState({
+                  userNotFound: true
+                });
+              }
+            }
+          );
+        });
+      }
+    );
+  };
+
+  handleUserInputSubmit = e => {
+    if (e.keyCode === 13) {
+      this.submitUserInput();
+    }
+  };
+
+  render() {
+    if (this.state.profile) console.log(this.state.profile.role.split(","));
+    return this.state.profile ? (
+      <div className="home">
+        {" "}
+        {/* <button onClick={this.handleSave}>Save</button> */}{" "}
+        {/* top section */}{" "}
+        <section className="flex-center">
+          {" "}
+          {/* <h1 className="absolute text-black two-thirds">Hi!</h1> */}{" "}
+          <h1 className="two-thirds text-center m-auto">
+            I 'm {this.state.profile.fullName}{" "}
+          </h1>{" "}
+          <img
+            className="me fixed whole md-half"
+            src={images("./" + this.state.profile.picture)}
+            alt={this.state.profile.fullName}
+          />{" "}
+        </section>
+        {/* 'deseloper' */}{" "}
+        <section className="home-about-section bg-main flex-column flex-center text-center">
+          {" "}
+          {/* <div className="absolute whole md-half flex-center hidden lg-flex">
+                            <Wavy />
+                          </div> */}{" "}
+          <h1 className="absolute text-shadow-out text-main deseloper">
+            {" "}
+            {this.state.profile.role.split(",").length > 1
+              ? this.state.profile.role.split(",").map((item, i) => {
+                  if (i === 0) {
+                    return item;
+                  } else {
+                    return (
+                      <div>
+                        + <br /> {item}{" "}
+                      </div>
+                    );
+                  }
+                })
+              : this.state.profile.role}{" "}
+          </h1>{" "}
+          {/* <div className="absolute two-thirds md-half lg-third m-auto flex-center flex-column">
+                          <h4>I'm a generalist designer and front-end developer. </h4>
+                          <h2>Designer<br/> + <br/>Developer</h2>
+                          <h4>I can design anything, but I love making apps the most.</h4>
+                        </div> */}{" "}
+        </section>
+        {/* project section */}
+        <section className="relative bg-main flex-center flex-column lg-flex-row lg-pt30 lg-pb30">
+          <div
+            className="absolute whole md-two-thirds flex-column flex-center"
+            style={{
+              transform: "translateY(-50%)"
+            }}
+          >
+            <div className="whole absolute">
+              <ClicktoolCity />
+            </div>{" "}
+            <div
+              className="two-thirds mb30 relative"
+              style={{
+                transform: "translateY(-100%)"
+              }}
+            >
+              <ClicktoolLogo />
+            </div>{" "}
+          </div>
+          <div className="laptop-project-container flex-center whole md-half relative">
+            <img className="whole absolute" src={Laptop} />{" "}
+            <div className="relative laptop-project-video-container">
+              <video
+                className="laptop-project-video"
+                controls
+                loop
+                src={Clicktool}
+              />{" "}
+            </div>{" "}
+            <Link className="clicktool-project-button" to="/projects/clicktool">
+              <button> View Project </button>{" "}
+            </Link>{" "}
+          </div>{" "}
+        </section>{" "}
+        <section className="bg-main">
+          <div className="colors">
+            `{" "}
+            <div
+              className="bg-primary0"
+              style={{ height: 100, width: 100 }}
+            ></div>
+            <div
+              className="bg-secondary0"
+              style={{ height: 100, width: 100 }}
+            ></div>
+            <div
+              className="bg-tertiary0"
+              style={{ height: 100, width: 100 }}
+            ></div>
+            <div
+              className="bg-quaternary0"
+              style={{ height: 100, width: 100 }}
+            ></div>
+            `{" "}
+          </div>{" "}
+        </section>{" "}
+      </div>
+    ) : (
+      <div
+        style={{
+          minHeight: "calc(100vh - var(--navHeight))"
+        }}
+        className="flex-column flex-center"
+      >
+        <P className="two-thirds text-center m-auto"> Who are you ? </P>{" "}
+        <input
+          style={styles.userInput}
+          type="text"
+          value={this.state.name}
+          onChange={this.handleUserInput}
+          onKeyUp={this.handleUserInputSubmit}
+        />{" "}
+        <p
+          style={
+            this.state.userNotFound
+              ? {
+                  color: "red",
+                  marginTop: 0
+                }
+              : {
+                  display: "none"
+                }
+          }
+        >
+          User not found : ({" "}
+        </p>{" "}
+        {this.state.userNotFound ? (
+          <Link to="/register">
+            <button> Register </button>{" "}
+          </Link>
+        ) : (
+          <button onClick={this.submitUserInput}> Submit </button>
+        )}{" "}
+      </div>
+    );
+  }
+}
+
+export default Home;
--- front-end-client/src/COMPONENTS/NAV/Nav.js
+++ front-end-client/src/COMPONENTS/NAV/Nav.js
@@ -1,42 +1,48 @@
 // Nav.js
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import "./nav.css";
 import Logo from "../../IMG/CHULPS_LOGO_ANIMATION_instagram.gif";
 import LogoWhite from "../../IMG/CHULPS_LOGO_ANIMATION_WHITE_NO_BG_portfolio.gif";
-
 
 class Nav extends React.Component {
   state = {
     displaymobileNav: false,
     menuText: "Menu"
   };
-  
+
   render() {
     return (
       // Nav.js
 
       <nav>
-
         {/* wrapper for the logo and logo text, hidden on small screens */}
         <Link to="/">
           <span className="logoWrapper">
-            <div style={{width: '4em', height: '4em'}}>
+            <div style={{ width: "4em", height: "4em" }}>
               <img
-              className="logo"
-              src={Logo}
-              alt="Mr. Charles" style={{objectFit:'contain'}}/>
+                className="logo"
+                src={Logo}
+                alt="Mr. Charles"
+                style={{ objectFit: "contain" }}
+              />
             </div>
-            <h5 
-            style={{margin: 0}}
-            className="hidden md-flex font-condensed font-weight-regular">
-                Chuck Howard</h5>
+            <h5
+              style={{ margin: 0 }}
+              className="hidden md-flex font-condensed font-weight-regular"
+            >
+              Chuck Howard
+            </h5>
           </span>
-          </Link>
+        </Link>
         {/* desktop nav menu */}
         <ul className="hidden md-flex">
-          <li><Link to="/about">WORK</Link></li>
-          <li><Link to="/works">RESUME</Link></li>
+          <li>
+            <Link to="/about">RESUME</Link>
+          </li>
+          <li>
+            <Link to="/works">WORK</Link>
+          </li>
           {/* <Link to="/contact"><button>Contact</button></Link> */}
         </ul>
 
@@ -51,13 +57,15 @@ class Nav extends React.Component {
           onClick={() =>
             this.setState({
               displaymobileNav: !this.state.displaymobileNav,
-              menuText: `${this.state.displaymobileNav === false ? "Close":"Menu"}`
+              menuText: `${
+                this.state.displaymobileNav === false ? "Close" : "Menu"
+              }`
             })
           }
         >
           {this.state.menuText}
         </button>
-        
+
         <div
           id="mobile-nav-links"
           className={
@@ -66,41 +74,60 @@ class Nav extends React.Component {
               : "mobileNav mobileNav-hide"
           }
         >
-          <div className="overlay md-hidden"              
-              onClick={() =>
-                this.setState({
-                  displaymobileNav: !this.state.displaymobileNav,
-                  menuText: `${this.state.displaymobileNav === false ? "Close":"Menu"}`
-                })
-              }>
+          <div
+            className="overlay md-hidden"
+            onClick={() =>
+              this.setState({
+                displaymobileNav: !this.state.displaymobileNav,
+                menuText: `${
+                  this.state.displaymobileNav === false ? "Close" : "Menu"
+                }`
+              })
+            }
+          >
             <img src={LogoWhite} className="absolute" alt="" />
-            
-            <div className="mobile-nav-links flex-column absolute flex-space-around" style={{height: '50%'}}>
+
+            <div
+              className="mobile-nav-links flex-column absolute flex-space-around"
+              style={{ height: "50%" }}
+            >
               <div
                 onClick={() =>
                   this.setState({
                     displaymobileNav: !this.state.displaymobileNav,
-                    menuText: `${this.state.displaymobileNav === false ? "Close":"Menu"}`
+                    menuText: `${
+                      this.state.displaymobileNav === false ? "Close" : "Menu"
+                    }`
                   })
-                }>
+                }
+              >
                 <Link to="/">Home</Link>
               </div>
               <div
                 onClick={() =>
                   this.setState({
                     displaymobileNav: !this.state.displaymobileNav,
-                    menuText: `${this.state.displaymobileNav === false ? "Close":"Menu"}`
+                    menuText: `${
+                      this.state.displaymobileNav === false ? "Close" : "Menu"
+                    }`
                   })
-                }><Link to="/about">WORK</Link></div>
+                }
+              >
+                <Link to="/about">RESUME</Link>
+              </div>
               <div
                 onClick={() =>
                   this.setState({
                     displaymobileNav: !this.state.displaymobileNav,
-                    menuText: `${this.state.displaymobileNav === false ? "Close":"Menu"}`
+                    menuText: `${
+                      this.state.displaymobileNav === false ? "Close" : "Menu"
+                    }`
                   })
-                }><Link to="/clicktool">RESUME</Link></div>
-
+                }
+              >
+                <Link to="/clicktool">WORK</Link>
               </div>
+            </div>
           </div>
         </div>
       </nav>
