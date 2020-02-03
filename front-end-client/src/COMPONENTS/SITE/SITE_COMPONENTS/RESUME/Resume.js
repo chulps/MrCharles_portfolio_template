94b3c048b30ccb433225cf602a229b4257d33c18
Chulps <chuckoward@gmail.com>
2020-02-03 10:12:10 -0800

    resume page fixes

--- front-end-client/src/COMPONENTS/SITE/SITE_COMPONENTS/RESUME/Resume.js
+++ front-end-client/src/COMPONENTS/SITE/SITE_COMPONENTS/RESUME/Resume.js
@@ -28,10 +28,10 @@ class Resume extends React.Component {
         >
           Download PDF
         </button>
-        <section className="mobileResume md-hidden pr30 pb30 pl30">
-          <div className="flex-column flex-space-between">
+        <section className="main-resume">
+          <div className="one-column md-three-columns lg-gap10">
             <div className="general-info mb20">
-              <h4 className="name">CHUCK HOWARD</h4>
+              <h5 className="name font-condensed">CHUCK HOWARD</h5>
               <div className="title">Designer + Developer</div>
             </div>
 
@@ -46,34 +46,30 @@ class Resume extends React.Component {
 
             <div className="location-info mb20">
               <label>LOCATION</label>
-              <div className="city">Los Angeles</div>
+              <div className="city">Los Angeles,</div>
               <div className="state">California</div>
             </div>
           </div>
 
-          <div className="intro mb20 flex-column">
+          <div className="intro mb20 flex-column whole">
             <label>statement</label>
-            Hello, I'm Chuck, I'm a designer, developer, and generally very
-            handy when you're trying to make something; Especially if you're
-            making an app! I'm looking for a new role where I lead the design of
-            digital products from top to bottom.
+            I'm a digital product designer, creative director and front-end
+            developer. I'm seeking a role where I can use the sum of my talents
+            to build apps, brands,software, and websites from top to bottom.
           </div>
 
           <div className="mb20">
             <label>SKILLS</label>
-            <div
-              className="flex-row flex-space-between"
-              style={{ flexWrap: "wrap" }}
-            >
+            <div className="flex-row flex-start" style={{ flexWrap: "wrap" }}>
               {skills.map(element => {
                 console.log(element.image);
                 return (
                   <div
                     key={element.id}
-                    className="text-center m5 flex-column flex-center mt30"
+                    className="text-center m5 flex-column flex-center"
                   >
                     <img
-                      height={36}
+                      height={24}
                       src={skillsImages("./" + element.image)}
                       style={{ opacity: ".5" }}
                       alt=""
@@ -85,16 +81,22 @@ class Resume extends React.Component {
             </div>
           </div>
 
+          <label>experience</label>
           <div className="experience mb20">
-            <label>experience</label>
-
             {/* begin experience-item */}
-            <div className="experience-item whole">
+            <div className="experience-item whole one-column md-three-columns md-gap10">
               <h5 className="employer">Clicktool</h5>
               <div className="job-title">
                 Creative Director/Product Designer
               </div>
               <div className="job-duration">Aug 2018 - Current</div>
+            </div>
+
+            {/* begin exp item */}
+            <div className="experience-item whole">
+              <h5 className="employer">BTFL</h5>
+              <div className="job-title">Designer</div>
+              <div className="job-duration">Mar 2017 - Aug 2018</div>
             </div>
           </div>
 
@@ -137,115 +139,6 @@ class Resume extends React.Component {
           </div>
         </section>
 
-        {/* desktop resume */}
-        <section className="resume hidden md-block">
-          <div className="whole flex-row flex-space-between">
-            <div className="general-info">
-              <h4 className="name">CHUCK HOWARD</h4>
-              <div className="title">Designer + Developer</div>
-            </div>
-
-            <div className="contact-info">
-              <label>CONTACT</label>
-
-              <div className="website">www.chuck-howard.com</div>
-              <div className="linkedIn-url">
-                www.linkedin.com/in/chuck-howard/
-              </div>
-            </div>
-
-            <div className="location-info">
-              <label>LOCATION</label>
-              <div className="city">Los Angeles</div>
-              <div className="state">California</div>
-            </div>
-          </div>
-
-          <div className="intro mt20 mb20">
-            <label>statement</label>
-            <div>
-              Hello, I'm Chuck, I'm a designer, developer, and generally very
-              handy when you're trying to make something; Especially if you're
-              making an app! I'm looking for a new role where I lead the design
-              of digital products from top to bottom.
-            </div>
-          </div>
-
-          <div className="mb20">
-            <label>SKILLS</label>
-            <div
-              className="flex-row flex-space-between"
-              style={{ flexWrap: "wrap" }}
-            >
-              {skills.map(element => {
-                console.log(element.image);
-                return (
-                  <div key={element.id} className="m5 flex-column flex-center">
-                    <img
-                      height={24}
-                      src={skillsImages("./" + element.image)}
-                      alt=""
-                      style={{ opacity: ".5" }}
-                    />
-                    <label className="mt10">{element.title}</label>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-
-          <div className="experience mb20">
-            <label>experience</label>
-
-            {/* begin experience-item */}
-            <div className="experience-item whole three-columns gap20">
-              <div className="employer">Clicktool</div>
-              <div className="job-title">
-                Creative Director/Product Designer
-              </div>
-              <div className="job-duration">Aug 2018 - Current</div>
-            </div>
-          </div>
-
-          <div className="whole three-columns gap20">
-            <div>
-              <label>EDUCATION</label>
-              <div className="education-info flex-row">
-                {/* begin 1 education item */}
-                <div className="university-skills">university skills</div>
-                <div className="flex-column">
-                  <h5 className="university-name">University of Memphis</h5>
-                  <div className="degree-description">
-                    Bachelor degree: graphic design
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="contact-info">
-              <label>AWARDS</label>
-              <div className="education-info flex-row">
-                {/* begin 1 education item */}
-                <div className="university-skills">award icon</div>
-                <div className="flex-column">
-                  <h5 className="university-name">Gold Addy 2015</h5>
-                  <div className="degree-description">description</div>
-                </div>
-              </div>
-            </div>
-
-            <div className="location-info">
-              <label>REFERENCES</label>
-              <div className="references-info">
-                <h5 className="name">name</h5>
-                <div className="relationship">relationship</div>
-                <div className="phone-number">phone number</div>
-                <div className="email">phone number</div>
-              </div>
-            </div>
-          </div>
-        </section>
-
         {/* PDF resume */}
         <PDFExport
           paperSize={"Letter"}
@@ -258,7 +151,7 @@ class Resume extends React.Component {
           <section id="myResume" className="resume hidden md-block printing">
             <div className="whole flex-row flex-space-between">
               <div className="general-info">
-                <h4 className="name">CHUCK HOWARD</h4>
+                <h5 className="name">CHUCK HOWARD</h5>
                 <div className="title">Designer + Developer</div>
               </div>
 
@@ -273,19 +166,19 @@ class Resume extends React.Component {
 
               <div className="location-info">
                 <label>LOCATION</label>
-                <div className="city">Los Angeles</div>
+                <div className="city">Los Angeles,</div>
                 <div className="state">California</div>
               </div>
             </div>
 
             <div className="intro mt20 mb20">
-              Hello, I'm Chuck, I'm a designer, developer, and generally very
-              handy when you're trying to make something; Especially if you're
-              making an app! I'm looking for a new role where I lead the design
-              of digital products from top to bottom.
+              I'm a digital product designer, creative director and front-end
+              developer. I'm seeking a role where I can use the sum of my
+              talents to build apps, brands,software, and websites from top to
+              bottom.
             </div>
 
-            <div className="mb20">
+            {/* <div className="mb20">
               <label>SKILLS</label>
               <div
                 className="flex-row flex-space-between p30"
@@ -308,18 +201,25 @@ class Resume extends React.Component {
                   );
                 })}
               </div>
-            </div>
+            </div> */}
 
             <div className="experience mb20">
               <label>experience</label>
 
               {/* begin experience-item */}
               <div className="experience-item whole three-columns gap20">
-                <div className="employer">Clicktool</div>
+                <h5 className="employer">Clicktool</h5>
                 <div className="job-title">
                   Creative Director/Product Designer
                 </div>
                 <div className="job-duration">Aug 2018 - Current</div>
+              </div>
+
+              {/* begin exp item */}
+              <div className="experience-item whole three-columns gap20">
+                <div className="employer">BTFL</div>
+                <div className="job-title">Designer</div>
+                <div className="job-duration">Mar 2017 - Aug 2018</div>
               </div>
             </div>
 
